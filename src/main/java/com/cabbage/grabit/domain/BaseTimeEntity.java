9fdeef75ea1f300f5c2ebc1532a576b08bac4b3e
Thom <hsbang.thom@gmail.com>
2021-12-09 10:28:52 +0900

    base time entity

---
+++ src/main/java/com/cabbage/grabit/domain/BaseTimeEntity.java
@@ -0,0 +1,25 @@
+package com.cabbage.grabit.domain;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseTimeEntity {
+
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
+    @CreatedDate
+    private LocalDateTime createdDate;
+
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
+    @LastModifiedDate
+    private LocalDateTime modifiedDate;
+}
